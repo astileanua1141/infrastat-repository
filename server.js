c78b2506423c579cc8f925d8b640c85e839d8f8f
Alexandra Astileanu <Alexandra.Astileanu@gmail.com>
2022-09-11 13:37:06 +0300

    Index page

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const expressLayouts = require('express-ejs-layouts')
 
+app.set('view engine','ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+//where css, style files are:
+app.use(express.static('public'))
+
+// Database config
+const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
 const db = mongoose.connection
@@ -13,10 +25,12 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
 
 const catalogRouter = require('./routes/catalog')
 app.use('/catalog', catalogRouter)
 // localhost:3000/catalog/anything
 
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'))
